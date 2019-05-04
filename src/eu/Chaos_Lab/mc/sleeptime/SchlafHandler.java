d2d1317093d82713c60f4db9a84074ddda5468fb
Wurstfresser <frederik.roesler@gmail.com>
2019-05-04 21:53:50 +0200

    Updated Handler

--- src/eu/Chaos_Lab/mc/sleeptime/SchlafHandler.java
+++ src/eu/Chaos_Lab/mc/sleeptime/SchlafHandler.java
@@ -1,13 +1,30 @@
 package eu.Chaos_Lab.mc.sleeptime;
 
+import java.util.Random;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
 
 
 public class SchlafHandler implements Listener {
- public void PeePee() {
-	 System.out.println("PeePeePooPoo");
- }
+	
+ @EventHandler
+ public void OnSleep(PlayerBedLeaveEvent e) {
+	 Player p = e.getPlayer();
+	 p.sendMessage("Good Morning " + p.getDisplayName()); 
+	 
+	 Random r = new Random();
+	 int ir = r.nextInt(2);
+	 p.sendMessage(ir + "");
+	 /*switch() {
+	  
 
+	 }*/
+ }
+ 
+	
 	
 
 }
