fdf6ca1ffd4aa71fbe9a67a82555cae851c16788
Wurstfresser <frederik.roesler@gmail.com>
2019-05-04 21:32:24 +0200

    Updated SchlafHandler

--- src/eu/Chaos_Lab/mc/sleeptime/SchlafHandler.java
+++ src/eu/Chaos_Lab/mc/sleeptime/SchlafHandler.java
@@ -1,8 +1,12 @@
 package eu.Chaos_Lab.mc.sleeptime;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
 
-public class SchlafHandler implements Handler {
+
+public class SchlafHandler implements Listener {
+ public void PeePee() {
+	 System.out.println("PeePeePooPoo");
+ }
 
 	
 
