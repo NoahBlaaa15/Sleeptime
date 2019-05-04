46e222fce7e0cbbb085a8213998c2d4ed6c5ec15
Wurstfresser <frederik.roesler@gmail.com>
2019-05-04 22:48:01 +0200

    Updated SchlafHandler

--- src/eu/Chaos_Lab/mc/sleeptime/SchlafHandler.java
+++ src/eu/Chaos_Lab/mc/sleeptime/SchlafHandler.java
@@ -6,6 +6,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 
 public class SchlafHandler implements Listener {
@@ -17,13 +19,30 @@ public class SchlafHandler implements Listener {
 	 
 	 Random r = new Random();
 	 int ir = r.nextInt(2);
-	 p.sendMessage(ir + "");
-	 /*switch() {
+	 switch(ir) {
+	 	case 0:
+	 			GoodSleep(p);
+	 			
+	 		break;
+	 	case 1:
+	 			BadSleep(p);
+	 			
+	 		break;
+	 		
 	  
 
-	 }*/
+	 }
+ }
+
+ public void GoodSleep(Player p) {
+	 p.sendMessage("You had a very good night!");
+	 p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION,2400,1));
  }
  
+ public void BadSleep(Player p) {
+	 p.sendMessage("You had a very bad night!");
+	 p.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS,2400,1));
+ }
 	
 	
 
