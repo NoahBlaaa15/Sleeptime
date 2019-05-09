55c72a73bca98a5377a8d23cb218dbcfdc953cee
Wurstfresser <frederik.roesler@gmail.com>
2019-05-09 19:19:21 +0200

    Updated Schlaf

--- src/eu/Chaos_Lab/mc/sleeptime/SchlafHandler.java
+++ src/eu/Chaos_Lab/mc/sleeptime/SchlafHandler.java
@@ -2,6 +2,8 @@ package eu.Chaos_Lab.mc.sleeptime;
 
 import java.util.Random;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,7 +20,7 @@ public class SchlafHandler implements Listener {
 	 p.sendMessage("Good Morning " + p.getDisplayName()); 
 	 
 	 Random r = new Random();
-	 int ir = r.nextInt(2);
+	 int ir = r.nextInt(4);
 	 switch(ir) {
 	 	case 0:
 	 			GoodSleep(p);
@@ -27,6 +29,12 @@ public class SchlafHandler implements Listener {
 	 	case 1:
 	 			BadSleep(p);
 	 			
+	 		break;
+	 	case 2:
+	 			NormalSleep(p);
+	 		break;
+	 	case 3:
+	 			Nightmare(p);
 	 		break;
 	 		
 	  
@@ -44,6 +52,17 @@ public class SchlafHandler implements Listener {
 	 p.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS,2400,1));
  }
 	
+ public void NormalSleep(Player p) {
+	 p.sendMessage("You had a restful night!");
+ }
 	
+public void Nightmare(Player p) {
+	p.sendMessage("You had a horrible night!");
+	for(int i = (int) p.getLocation().getY(); i < 256; i++){
+	     if (p.getWorld().getBlockAt(new Location(p.getWorld(), p.getLocation().getX() + 200, i, p.getLocation().getZ() + 100)).getType().equals(Material.AIR)) {p.teleport(new Location(p.getWorld(), p.getLocation().getX() + 200, i, p.getLocation().getZ() + 100));
+	    		 }
+	}
+}
+
 
 }
